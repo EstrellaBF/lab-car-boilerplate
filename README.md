9152f774bc49c15219a4167b7deab7ff74ea43a7
EstrellaBF <stern.von.nord@gmail.com>
2017-12-17 17:08:58 -0500

    aplicando js y subiendo readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Realizando web LabCar
+
+
+# Objetivo 
+Realizar una web responsive usando bootstrap, la cual se vea según las indicaciones de vista de smartphone y laptop.
+
+## Características
+- Se verifica la versión móvil.
+- Se verifica la versión laptop.
+- Se implementa para tablet.
+- Cumple con los 3 modales solicitados.
+- Se utiliza las funcionalidades de bootstrap.
+- Se prueba las funciones de jquery.
--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 // La funcionalidad de tu proyecto
+window.addEventListener('load', function() {
+  var btnLogina = document.getElementById('btn-login-modal'),
+    btnClose = document.getElementById('btn-close-modal'),
+    usernameAccount = document.getElementById('username-account'),
+    passwordAccount = document.getElementById('password-account'),
+    navbar = document.getElementById('navbar');
+
+  btnLogina.addEventListener('click', function(event) {
+  console.log(event.currentTarget); 
+  console.log(event.target);
+  console.log(usernameAccount.value);
+  console.log(passwordAccount);
+    if(usernameAccount.value!=="" && passwordAccount.value!=="") {
+      var newDiv = document.createElement('div');
+      var newP = document.createElement('p');
+      newDiv.appendChild(newP);
+      navbar.appendChild(newDiv);
+      newP.innerHTML = usernameAccount.value;
+      btnClose.classList.add('btn-success');
+      newDiv.classList.add();
+    }
+  });
+/*
+  $(document).ready(function(){
+    $(".btn-download-mobile").mouseover(function(){
+        $(".full-width").slideUp();
+    });
+});*/
+
+});
+
